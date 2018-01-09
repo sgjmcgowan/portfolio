a5ce29d71eb70273fd22e91896ef06bb411ba898
Trevor Van Rys <trevorvanrys@trevors-mbp-2.fanshawec.ca>
2018-01-09 11:13:31 -0500

    initial commit

---
+++ scripts/contentObject.js
@@ -0,0 +1,23 @@
+var dynamicContent = {
+		windsor: {
+
+			images : [
+				"uwindsor_logo.png"
+			],
+
+			imageDescription : [
+				'ENGLISH and CREATIVE WRITING'
+			]
+		},
+
+		summer : {
+
+			images : [
+				"fanshawe_logo.png"
+			],
+
+			imageDescription : [
+				'INTERACTIVE MEDIA DESIGN'
+			]
+		}
+	};
